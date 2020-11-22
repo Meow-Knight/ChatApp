859ecb78380b027b281d79ef84d2dcd4970ef29c
nglan <nglan112000@gmail.com>
2020-11-22 10:09:26 +0700

    Add delete-textmessage and display-imagemessage feature

--- app/src/main/java/com/lecaoviethuy/messengerapp/AdapterClasses/ChatsAdapter.kt
+++ app/src/main/java/com/lecaoviethuy/messengerapp/AdapterClasses/ChatsAdapter.kt
@@ -1,17 +1,24 @@
 package com.lecaoviethuy.messengerapp.AdapterClasses
 
+import android.app.AlertDialog
 import android.content.Context
-import android.util.Log
+import android.content.DialogInterface
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import com.lecaoviethuy.messengerapp.R
+import com.lecaoviethuy.messengerapp.ViewFullImageActivity
+import com.lecaoviethuy.messengerapp.WelcomeActivity
 import com.lecaoviethuy.messengerapp.modelClasses.Chat
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
@@ -22,101 +29,153 @@ class ChatsAdapter (
     imageUrl: String
     ) : RecyclerView.Adapter<ChatsAdapter.ViewHolder?>() {
 
-    private val mContext: Context;
-    private val mChatList: List<Chat>;
-    private val imageUrl: String;
+    private val mContext: Context
+    private val mChatList: List<Chat>
+    private val imageUrl: String
 
-    var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser!!;
+    var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser!!
 
     init {
-        this.mChatList = mChatList;
-        this.mContext = mContext;
-        this.imageUrl = imageUrl;
+        this.mChatList = mChatList
+        this.mContext = mContext
+        this.imageUrl = imageUrl
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var profile_image: CircleImageView? = null;
-        var show_text_message: TextView? = null;
-        var left_image_view: ImageView? = null;
-        var right_image_view: ImageView? = null;
-        var text_seen: TextView? = null;
+        var profileImage: CircleImageView? = null
+        var showTextMessage: TextView? = null
+        var leftImageView: ImageView? = null
+        var rightImageView: ImageView? = null
+        var textSeen: TextView? = null
 
         init {
-            profile_image = itemView.findViewById(R.id.profile_image);
-            show_text_message = itemView.findViewById(R.id.show_text_message);
-            left_image_view = itemView.findViewById(R.id.left_image_view);
-            text_seen = itemView.findViewById(R.id.text_seen);
-            right_image_view = itemView.findViewById(R.id.right_image_view);
+            profileImage = itemView.findViewById(R.id.profile_image)
+            showTextMessage = itemView.findViewById(R.id.show_text_message)
+            leftImageView = itemView.findViewById(R.id.left_image_view)
+            textSeen = itemView.findViewById(R.id.text_seen)
+            rightImageView = itemView.findViewById(R.id.right_image_view)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // viewType == 1 -> user sent
         return if (viewType == 1) {
-            val view : View  = LayoutInflater.from(mContext).inflate(R.layout.message_item_right, parent,false);
-            return ViewHolder(view);
+            val view : View  = LayoutInflater.from(mContext).inflate(R.layout.message_item_right, parent,false)
+            ViewHolder(view)
         } else { // viewType == 0 -> visit user sent
-            val view : View  = LayoutInflater.from(mContext).inflate(R.layout.message_item_left, parent,false);
-            return ViewHolder(view);
+            val view : View  = LayoutInflater.from(mContext).inflate(R.layout.message_item_left, parent,false)
+            ViewHolder(view)
         }
     }
 
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val chat: Chat = mChatList[position];
+        val chat: Chat = mChatList[position]
 
         Picasso.get()
             .load(imageUrl)
             .placeholder(R.drawable.avatar)
-            .into(holder.profile_image);
+            .into(holder.profileImage)
 
         // image message
         if (chat.getMessage().equals("has sent you an image.") && !chat.getUrl().equals("")) {
             // image message - right side
+            // show full image when click on it
+            // show dialog delete when long click on it
             if (chat.getSender().equals(firebaseUser!!.uid)) {
-                holder.show_text_message!!.visibility = View.GONE;
-                holder.right_image_view!!.visibility = View.VISIBLE;
-                Picasso.get().load(chat.getUrl()).into(holder.right_image_view)
+                holder.showTextMessage!!.visibility = View.GONE
+                holder.rightImageView!!.visibility = View.VISIBLE
+                Picasso.get().load(chat.getUrl()).into(holder.rightImageView)
+
+                holder.rightImageView!!.setOnLongClickListener{
+                    val options = arrayOf<CharSequence>(
+                            "Delete Image",
+                            "Cancel"
+                    )
+
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(holder.itemView.context)
+                    builder.setTitle("What do you want?")
+                    builder.setItems(options) { _, i ->
+                        when(i){
+                            0 -> deleteSentMessage(position, holder)
+                        }
+                    }
+                    builder.show()
+                    true
+                }
+
+                holder.rightImageView!!.setOnClickListener{
+                    val intent = Intent(mContext, ViewFullImageActivity::class.java)
+                    intent.putExtra("url", chat.getUrl())
+                    mContext.startActivity(intent)
+                }
             }
 
             // image message - left side
+            // show full image when click on it
             if (!chat.getSender().equals(firebaseUser!!.uid)) {
-                holder.show_text_message!!.visibility = View.GONE;
-                holder.left_image_view!!.visibility = View.VISIBLE;
-                Picasso.get().load(chat.getUrl()).into(holder.left_image_view)
+                holder.showTextMessage!!.visibility = View.GONE
+                holder.leftImageView!!.visibility = View.VISIBLE
+                Picasso.get().load(chat.getUrl()).into(holder.leftImageView)
+
+                holder.leftImageView!!.setOnClickListener{
+                    val intent = Intent(mContext, ViewFullImageActivity::class.java)
+                    intent.putExtra("url", chat.getUrl())
+                    mContext.startActivity(intent)
+                }
             }
         }
 
         // text message
         else {
-            holder.show_text_message!!.text = chat.getMessage();
+            holder.showTextMessage!!.text = chat.getMessage()
         }
 
         // sent and seen message
         if (position == mChatList.size - 1) {
             if (chat.getIsSeen()) {
-                holder.text_seen!!.text = "Seen";
+                holder.textSeen!!.text = "Seen"
 
                 if (chat.getMessage().equals("has sent you an image.") && !chat.getUrl().equals("")) {
-                    val layoutParams: RelativeLayout.LayoutParams? = holder.text_seen!!.layoutParams as RelativeLayout.LayoutParams?;
-                    layoutParams!!.setMargins(0, 245, 10, 0);
-                    holder.text_seen!!.layoutParams = layoutParams;
+                    val layoutParams: RelativeLayout.LayoutParams? = holder.textSeen!!.layoutParams as RelativeLayout.LayoutParams?
+                    layoutParams!!.setMargins(0, 245, 10, 0)
+                    holder.textSeen!!.layoutParams = layoutParams
                 }
             } else {
-                holder.text_seen!!.text = "Sent";
+                holder.textSeen!!.text = "Sent"
 
                 if (chat.getMessage().equals("has sent you an image.") && !chat.getUrl().equals("")) {
-                    val layoutParams: RelativeLayout.LayoutParams? = holder.text_seen!!.layoutParams as RelativeLayout.LayoutParams?;
-                    layoutParams!!.setMargins(0, 245, 10, 0);
-                    holder.text_seen!!.layoutParams = layoutParams;
+                    val layoutParams: RelativeLayout.LayoutParams? = holder.textSeen!!.layoutParams as RelativeLayout.LayoutParams?
+                    layoutParams!!.setMargins(0, 245, 10, 0)
+                    holder.textSeen!!.layoutParams = layoutParams
                 }
             }
         } else {
-            holder.text_seen!!.visibility = View.GONE;
+            holder.textSeen!!.visibility = View.GONE
+            if(firebaseUser!!.uid == chat.getSender()){
+                holder.showTextMessage!!.setOnClickListener{
+                    val options = arrayOf<CharSequence>(
+                            "Delete Message",
+                            "Cancel"
+                    )
+
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(holder.itemView.context)
+                    builder.setTitle("What do you want?")
+                    builder.setItems(options) { _, i ->
+                        when(i){
+                            0 -> {
+                                deleteSentMessage(position, holder)
+                            }
+                        }
+                    }
+                    builder.show()
+                }
+            }
         }
     }
 
     override fun getItemCount(): Int {
-        return mChatList.size;
+        return mChatList.size
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -126,5 +185,20 @@ class ChatsAdapter (
             1
         else
             0
+    }
+
+    private fun deleteSentMessage(position: Int, holder: ViewHolder){
+        val ref = FirebaseDatabase.getInstance().reference
+                .child("Chats")
+                .child(mChatList[position].getMessageId()!!)
+                .removeValue()
+                .addOnCompleteListener{
+                    task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(holder.itemView.context, "Deleted!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(holder.itemView.context, "Delete Failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 }
--- app/src/main/java/com/lecaoviethuy/messengerapp/MessageChatActivity.kt
+++ app/src/main/java/com/lecaoviethuy/messengerapp/MessageChatActivity.kt
@@ -25,23 +25,23 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_message_chat.*
 
 class MessageChatActivity : AppCompatActivity() {
-    var userIdVisit : String = "";
-    var firebaseUser : FirebaseUser ? = null;
-    var chatsAdapter : ChatsAdapter? = null;
-    var mChatList : List<Chat>? = null;
-    var reference : DatabaseReference? = null;
+    var userIdVisit : String = ""
+    var firebaseUser : FirebaseUser ? = null
+    var chatsAdapter : ChatsAdapter? = null
+    var mChatList : List<Chat>? = null
+    var reference : DatabaseReference? = null
 
-    lateinit var recycler_view_chats : RecyclerView;
+    lateinit var recycler_view_chats : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message_chat)
 
-        val toolbar : Toolbar = findViewById(R.id.toolbar_message_chat);
-        setSupportActionBar(toolbar);
+        val toolbar : Toolbar = findViewById(R.id.toolbar_message_chat)
+        setSupportActionBar(toolbar)
 
-        supportActionBar!!.title = "";
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true);
+        supportActionBar!!.title = ""
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener {
             val intent = Intent(this@MessageChatActivity, WelcomeActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -50,77 +50,77 @@ class MessageChatActivity : AppCompatActivity() {
         }
 
         // Get visit user ID
-        userIdVisit = intent.getStringExtra("visit_id")!!;
+        userIdVisit = intent.getStringExtra("visit_id")!!
         // Current user
-        firebaseUser = FirebaseAuth.getInstance().currentUser;
+        firebaseUser = FirebaseAuth.getInstance().currentUser
 
         // Recycler view message
-        recycler_view_chats = findViewById(R.id.recycler_view_chats);
-        recycler_view_chats.setHasFixedSize(true);
+        recycler_view_chats = findViewById(R.id.recycler_view_chats)
+        recycler_view_chats.setHasFixedSize(true)
 
-        var linearLayoutManager = LinearLayoutManager(applicationContext);
-        linearLayoutManager.stackFromEnd = true;
-        recycler_view_chats.layoutManager = linearLayoutManager;
+        val linearLayoutManager = LinearLayoutManager(applicationContext)
+        linearLayoutManager.stackFromEnd = true
+        recycler_view_chats.layoutManager = linearLayoutManager
 
         // visit User
         val reference = FirebaseDatabase.getInstance().reference
-            .child("Users").child(userIdVisit);
+            .child("Users").child(userIdVisit)
 
         reference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val user: User? = snapshot.getValue(User::class.java);
+                val user: User? = snapshot.getValue(User::class.java)
 
-                username_mchat.text = user!!.getUsername();
+                username_mchat.text = user!!.getUsername()
                 Picasso.get()
                     .load(user.getProfile())
                     .placeholder(R.drawable.avatar)
-                    .into(profile_image_mchat);
+                    .into(profile_image_mchat)
 
-                retrieveMessage(firebaseUser!!.uid, userIdVisit, user.getProfile());
+                retrieveMessage(firebaseUser!!.uid, userIdVisit, user.getProfile())
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@MessageChatActivity,"Can't get user profile!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this@MessageChatActivity,"Can't get user profile!", Toast.LENGTH_SHORT).show()
             }
         })
 
         // Add send btn event
         send_message_btn.setOnClickListener {
-            val message = text_message.text.toString();
+            val message = text_message.text.toString()
             if (message == "") {
                 Toast.makeText(this@MessageChatActivity, "Please write a message, please...", Toast.LENGTH_SHORT).show()
             } else {
-                sendMessageToUser(firebaseUser!!.uid, userIdVisit, message);
+                sendMessageToUser(firebaseUser!!.uid, userIdVisit, message)
             }
 
-            text_message.setText("");
+            text_message.setText("")
         }
 
         // Add image event
         attach_image_file_btn.setOnClickListener {
-            val intent = Intent();
-            intent.action = Intent.ACTION_GET_CONTENT;
-            intent.type = "image/*";
-            startActivityForResult(Intent.createChooser(intent,"Pick Image"), 438);
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(Intent.createChooser(intent,"Pick Image"), 438)
         }
 
-        seenMessage(userIdVisit);
+        seenMessage(userIdVisit)
     }
 
     // how to send message to user
     private fun sendMessageToUser(senderId: String, receiverId : String?, message: String) {
-        val reference = FirebaseDatabase.getInstance().reference;
+        val reference = FirebaseDatabase.getInstance().reference
         // new message
-        val messageKey = reference.push().key;
+        val messageKey = reference.push().key
 
         // body data
-        val messageHashMap = HashMap<String, Any?>();
-        messageHashMap["sender"] = senderId;
-        messageHashMap["message"] = message;
-        messageHashMap["receiver"] = receiverId;
-        messageHashMap["isSeen"] = false;
-        messageHashMap["url"] = "";
-        messageHashMap["messageId"] = messageKey;
+        val messageHashMap = HashMap<String, Any?>()
+        messageHashMap["sender"] = senderId
+        messageHashMap["message"] = message
+        messageHashMap["receiver"] = receiverId
+        messageHashMap["isSeen"] = false
+        messageHashMap["url"] = ""
+        messageHashMap["messageId"] = messageKey
 
         // add new message
         reference.child("Chats")
@@ -132,58 +132,58 @@ class MessageChatActivity : AppCompatActivity() {
                         .reference
                         .child("ChatList")
                         .child(firebaseUser!!.uid)
-                        .child(userIdVisit);
+                        .child(userIdVisit)
 
                     chatsListReference.addListenerForSingleValueEvent(object : ValueEventListener {
                         override fun onDataChange(snapshot: DataSnapshot) {
                             if (!snapshot.exists()) {
-                                chatsListReference.child("id").setValue(userIdVisit);
+                                chatsListReference.child("id").setValue(userIdVisit)
                             }
 
                             val chatsListReceiverRef = FirebaseDatabase.getInstance()
                                 .reference
                                 .child("ChatList")
                                 .child(userIdVisit)
-                                .child(firebaseUser!!.uid);
+                                .child(firebaseUser!!.uid)
 
-                            chatsListReceiverRef.child("id").setValue(firebaseUser!!.uid);
+                            chatsListReceiverRef.child("id").setValue(firebaseUser!!.uid)
                         }
 
                         override fun onCancelled(error: DatabaseError) {
-                            Toast.makeText(this@MessageChatActivity,"sent message error, try again...", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(this@MessageChatActivity,"sent message error, try again...", Toast.LENGTH_SHORT).show()
                         }
                     })
 
                     // change ID
-                    chatsListReference.child("id").setValue(firebaseUser!!.uid);
+                    chatsListReference.child("id").setValue(firebaseUser!!.uid)
 
                     // implement the push notification using fcm
 
                     val reference = FirebaseDatabase.getInstance().reference
-                        .child("Users").child(firebaseUser!!.uid);
+                        .child("Users").child(firebaseUser!!.uid)
                 }
-            };
+            }
     }
 
     // handle after intent
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == 438 && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
             // show progressbar
             val progressBar = ProgressDialog(this)
             progressBar.setMessage("image is uploading, please wait...")
-            progressBar.show();
+            progressBar.show()
 
             // upload chat image
-            val fileUri = data.data;
-            val storageReference =  FirebaseStorage.getInstance().reference.child("Chat Images");
-            val ref = FirebaseDatabase.getInstance().reference;
-            val messageId = ref.push().key;
-            val filePath = storageReference.child("$messageId.jpg");
+            val fileUri = data.data
+            val storageReference =  FirebaseStorage.getInstance().reference.child("Chat Images")
+            val ref = FirebaseDatabase.getInstance().reference
+            val messageId = ref.push().key
+            val filePath = storageReference.child("$messageId.jpg")
 
-            var uploadTask : StorageTask<*>
-            uploadTask = filePath.putFile(fileUri!!);
+            val uploadTask : StorageTask<*>
+            uploadTask = filePath.putFile(fileUri!!)
 
             uploadTask.continueWithTask(Continuation <UploadTask.TaskSnapshot, Task<Uri>>{ task ->
                 if (task.isSuccessful){
@@ -191,67 +191,67 @@ class MessageChatActivity : AppCompatActivity() {
                         throw it
                     }
                 }
-                return@Continuation filePath.downloadUrl;
+                return@Continuation filePath.downloadUrl
             }).addOnCompleteListener {task ->
                 if (task.isSuccessful){
                     val downloadUri = task.result
-                    val url = downloadUri.toString();
+                    val url = downloadUri.toString()
 
-                    val messageHashMap = HashMap<String, Any?>();
-                    messageHashMap["sender"] = firebaseUser!!.uid;
-                    messageHashMap["message"] = "has sent you an image.";
-                    messageHashMap["receiver"] = userIdVisit;
-                    messageHashMap["isSeen"] = false;
-                    messageHashMap["url"] = url;
-                    messageHashMap["messageId"] = messageId;
+                    val messageHashMap = HashMap<String, Any?>()
+                    messageHashMap["sender"] = firebaseUser!!.uid
+                    messageHashMap["message"] = "has sent you an image."
+                    messageHashMap["receiver"] = userIdVisit
+                    messageHashMap["isSeen"] = false
+                    messageHashMap["url"] = url
+                    messageHashMap["messageId"] = messageId
 
-                    ref.child("Chats").child(messageId!!).setValue(messageHashMap);
+                    ref.child("Chats").child(messageId!!).setValue(messageHashMap)
 
-                    progressBar.dismiss();
+                    progressBar.dismiss()
                 }
-            };
+            }
         }
     }
 
     private fun retrieveMessage(senderId: String, receiverId: String?, receiverImageUrl: String?) {
-        mChatList = ArrayList();
-        val reference = FirebaseDatabase.getInstance().reference.child("Chats");
+        mChatList = ArrayList()
+        val reference = FirebaseDatabase.getInstance().reference.child("Chats")
 
         reference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                (mChatList as ArrayList<Chat>).clear();
+                (mChatList as ArrayList<Chat>).clear()
                 for (shot in snapshot.children) {
-                    val chat = shot.getValue(Chat::class.java);
+                    val chat = shot.getValue(Chat::class.java)
 
                     if (chat!!.getReceiver().equals(senderId) && chat.getSender().equals(receiverId)
                         || chat.getReceiver().equals(receiverId) && chat.getSender().equals(senderId)) {
-                        (mChatList as ArrayList<Chat>).add(chat);
+                        (mChatList as ArrayList<Chat>).add(chat)
                     }
 
-                    chatsAdapter = ChatsAdapter(this@MessageChatActivity, mChatList as ArrayList<Chat>, receiverImageUrl!!);
-                    recycler_view_chats.adapter = chatsAdapter;
+                    chatsAdapter = ChatsAdapter(this@MessageChatActivity, mChatList as ArrayList<Chat>, receiverImageUrl!!)
+                    recycler_view_chats.adapter = chatsAdapter
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@MessageChatActivity, error.message, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this@MessageChatActivity, error.message, Toast.LENGTH_SHORT).show()
             }
         })
     }
 
-    var seenListener: ValueEventListener? = null;
+    var seenListener: ValueEventListener? = null
     private fun seenMessage(userId: String) {
-        reference = FirebaseDatabase.getInstance().reference.child("Chats");
+        reference = FirebaseDatabase.getInstance().reference.child("Chats")
 
         seenListener = reference!!.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 for (shot in snapshot.children) {
-                    val chat = shot.getValue(Chat::class.java);
+                    val chat = shot.getValue(Chat::class.java)
 
-                    if (chat!!.getReceiver().equals(firebaseUser!!.uid) && chat!!.getSender().equals(userId)) {
-                        val hashMap = HashMap<String, Any>();
-                        hashMap["isSeen"] = true;
-                        shot.ref.updateChildren(hashMap);
+                    if (chat!!.getReceiver().equals(firebaseUser!!.uid) && chat.getSender().equals(userId)) {
+                        val hashMap = HashMap<String, Any>()
+                        hashMap["isSeen"] = true
+                        shot.ref.updateChildren(hashMap)
                     }
                 }
             }
@@ -263,8 +263,8 @@ class MessageChatActivity : AppCompatActivity() {
     }
 
     override fun onPause() {
-        super.onPause();
+        super.onPause()
 
-        reference!!.removeEventListener(seenListener!!);
+        reference!!.removeEventListener(seenListener!!)
     }
 }
